b544265879d8742d547e5b63fadee0dba952e8fd
ttmpt <ttmpt@gmx.net>
2026-01-16 19:40:07 +0100

    feat: add sound effects

--- src/main/java/net/ttmpt/DarknessHasTeeth.java
+++ src/main/java/net/ttmpt/DarknessHasTeeth.java
@@ -10,24 +10,11 @@ public class DarknessHasTeeth extends JavaPlugin {
 
     public DarknessHasTeeth(@Nonnull JavaPluginInit init) {
         super(init);
-        logToFile("[DarkTimes] Plugin constructed");
     }
 
     @Override
     protected void setup() {
         super.setup();
-        logToFile("[DarkTimes] setup() called");
-
         this.getEntityStoreRegistry().registerSystem(new DarknessMiningSystem());
-        logToFile("[DarkTimes] DarkMiningSystem registered");
-    }
-
-    public static void logToFile(String message) {
-        try (FileWriter fw = new FileWriter("/tmp/darktimes.log", true)) {
-            fw.write(message + "\n");
-            fw.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/net/ttmpt/DarknessMiningSystem.java
+++ src/main/java/net/ttmpt/DarknessMiningSystem.java
@@ -1,52 +1,63 @@
 package net.ttmpt;
 
-import com.hypixel.hytale.math.util.ChunkUtil;
+import com.hypixel.hytale.protocol.SoundCategory;
 import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
-import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.EntityEventSystem;
-import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.math.util.ChunkUtil;
+import com.hypixel.hytale.math.util.ChunkUtil;
 import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.math.vector.Vector3f;
+import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.protocol.ColorLight;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
+import com.hypixel.hytale.server.core.asset.type.item.config.Item;
+import com.hypixel.hytale.server.core.asset.type.soundevent.config.SoundEvent;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.ecs.BreakBlockEvent;
-import com.hypixel.hytale.server.core.universe.world.World;
-import com.hypixel.hytale.server.core.universe.PlayerRef;
-import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
-import com.hypixel.hytale.server.core.universe.world.chunk.BlockChunk;
-import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
-import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.inventory.Inventory;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
-import com.hypixel.hytale.server.core.asset.type.item.config.Item;
-import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
 import com.hypixel.hytale.server.core.modules.entity.component.DynamicLight;
-import com.hypixel.hytale.protocol.ColorLight;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.SoundUtil;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.chunk.BlockChunk;
+import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Map;
-import java.util.HashMap;
 
 public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBlockEvent> {
-    private static final int THRESHOLD_NOTICE = 100;
-    private static final int THRESHOLD_WARN = 150;
-    private static final int THRESHOLD_SPAWN = 200;
-    private static final double INCREASE_DARKNESS_CHANCE = 0.3;
+    private static final int THRESHOLD_NOTICE = 20;
+    private static final int THRESHOLD_WARN = 25;
+    private static final int THRESHOLD_SPAWN = 30;
+    private static final double INCREASE_DARKNESS_CHANCE = 0.5;
 
-    private final String[] creatures = new String[]{"Edible_Rat", "Rat", "Scarak_Louse"};
+    private final String[] creatures = new String[]{
+        "Edible_Rat", "Rat", "Scarak_Louse", "Crawler_Void"
+    };
+
+    private final String[] sounds = new String[]{
+        "SFX_Crawler_Void_Alerted",
+        "SFX_Crawler_Void_Alerted_02",
+        "SFX_Crawler_Void_Death"
+    };
 
     private Map<Integer, PlayerDarkness> players = new HashMap<>();
     private final Random random = new Random();
 
     public DarknessMiningSystem() {
         super(BreakBlockEvent.class);
-        DarknessHasTeeth.logToFile("[DarkMiningSystem] Initialized");
     }
 
     @Override
@@ -59,20 +70,17 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         if (blockId.equals("Empty")) {
             return;
         }
-        DarknessHasTeeth.logToFile("[Dark] Mining " + blockId + " at " + blockPos);
 
         // Get player
         Ref<EntityStore> playerRef = archetypeChunk.getReferenceTo(index);
         Player player = store.getComponent(playerRef, Player.getComponentType());
         if (player == null) {
-            DarknessHasTeeth.logToFile("[Dark] Player is null");
             return;
         }
 
         // Get world
         World world = player.getWorld();
         if (world == null) {
-            DarknessHasTeeth.logToFile("[Dark] World is null");
             return;
         }
 
@@ -89,7 +97,6 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         long deltaTick = currentTick - darkness.lastBreakTick;
         darkness.lastBreakTick = currentTick;
 
-        DarknessHasTeeth.logToFile("[Dark] elapsed ticks: " + deltaTick);
         if (deltaTick > 2000) {
             darkness.darknessLevel = 0;
         }
@@ -97,12 +104,10 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         // Get player pos. player Y must be similar to block.
         TransformComponent playerTransform = store.getComponent(playerRef, TransformComponent.getComponentType());
         if (playerTransform == null) {
-            DarknessHasTeeth.logToFile("[Dark] Player missing pos");
             return;
         }
         Vector3d playerPos = playerTransform.getPosition();
         if (Math.abs(blockPos.getY() - playerPos.getY()) > 1) {
-            DarknessHasTeeth.logToFile("[Dark] Player wrong y level");
             darkness.darknessLevel = 0;
             return;
         }
@@ -113,26 +118,20 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         if (firstItem != null) {
             BlockType blocktype = BlockType.getAssetMap().getAsset(firstItem.getBlockKey());
             if (blocktype != null) {
-                DarknessHasTeeth.logToFile("[Dark] item has block");
                 ColorLight itemLight = blocktype.getLight();
                 if (itemLight != null) {
-                    DarknessHasTeeth.logToFile("[Dark] item block has light");
                     darkness.darknessLevel = 0;
                     return;
                 }
             }
-        } else {
-            DarknessHasTeeth.logToFile("[Dark] no first item");
         }
 
         ItemStack secondItem = inventory.getUtilityItem();
         if (secondItem != null) {
             BlockType blocktype = BlockType.getAssetMap().getAsset(secondItem.getBlockKey());
             if (blocktype != null) {
-                DarknessHasTeeth.logToFile("[Dark] item has block");
                 ColorLight itemLight = blocktype.getLight();
                 if (itemLight != null) {
-                    DarknessHasTeeth.logToFile("[Dark] item block has light");
                     darkness.darknessLevel = 0;
                     return;
                 }
@@ -142,7 +141,6 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         // Check if there is blockLight
         byte blockLight = getBlockLight(world, blockPos);
         if (blockLight != 0) {
-            DarknessHasTeeth.logToFile("[Dark] Has blocklight");
             darkness.darknessLevel = 0;
             return;
         }
@@ -153,19 +151,17 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         }
 
         darkness.darknessLevel++;
-        DarknessHasTeeth.logToFile("[Dark] darkness level " + darkness.darknessLevel);
 
         if (darkness.darknessLevel > THRESHOLD_SPAWN) {
             player.sendMessage(Message.raw("A shadowy creature has spawned from the darkness!"));
             spawnCreature(player.getWorld(), blockPos);
-            darkness.darknessLevel = -100;
-            DarknessHasTeeth.logToFile("[Dark] spawned");
+            darkness.darknessLevel = -10;
+            playSound(world, store, playerRef, playerPos, getSound());
         } else if (darkness.darknessLevel == THRESHOLD_WARN) {
             player.sendMessage(Message.raw("You can hear sounds from the darkness itself... Quick, return to the light!"));
-            DarknessHasTeeth.logToFile("[Dark] warned");
+            playSound(world, store, playerRef, playerPos, getSound());
         } else if (darkness.darknessLevel == THRESHOLD_NOTICE) {
             player.sendMessage(Message.raw("It is pitch black, you are likely to be eaten by something..."));
-            DarknessHasTeeth.logToFile("[Dark] notice");
         }
     }
 
@@ -175,6 +171,7 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         PlayerDarkness() {}
     }
 
+    @Nonnull
     private String getCreature() {
         int index = random.nextInt(creatures.length);
         return creatures[index];
@@ -187,13 +184,11 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         long chunkIndex = ChunkUtil.indexChunkFromBlock(blockPos.getX(), blockPos.getZ());
         WorldChunk chunk = world.getChunk(chunkIndex);
         if (chunk == null) {
-            DarknessHasTeeth.logToFile("[Dark] Chunk not loaded for block light");
             return 15;
         }
 
         BlockChunk blockChunk = chunk.getBlockChunk();
         if (blockChunk == null) {
-            DarknessHasTeeth.logToFile("[Dark] Block chunk is null for block light");
             return 15;
         }
 
@@ -204,7 +199,7 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
         byte skyLight = blockChunk.getSkyLight(localX, localY, localZ);
         byte blockLight = blockChunk.getBlockLightIntensity(localX, localY, localZ);
 
-        return (byte) Math.max(skyLight, blockLight);
+        return (byte) Math.max(skyLight, blockLight - 2);
     }
 
     private void spawnCreature(World world, Vector3i blockPos) {
@@ -217,28 +212,31 @@ public class DarknessMiningSystem extends EntityEventSystem<EntityStore, BreakBl
                 spawnPos.getY() + 0.1,
                 spawnPos.getZ() + 0.5
             );
-            // Vector3f rotation = Vector3f.FORWARD;
             Vector3f rotation = new Vector3f(0, 0, 0);
 
             com.hypixel.hytale.server.npc.NPCPlugin npcPlugin = com.hypixel.hytale.server.npc.NPCPlugin.get();
             int roleIndex = npcPlugin.getIndex(getCreature());
 
             if (roleIndex == Integer.MIN_VALUE) {
-                DarknessHasTeeth.logToFile("[Dark] Failed to find role!");
                 return;
             }
 
-            try {
-                var pair = npcPlugin.spawnEntity(store, roleIndex, position, rotation, null, null);
-                if (pair != null && pair.first() != null) {
-                    DarknessHasTeeth.logToFile("[Dark] Mob spawned at: " + position);
-                } else {
-                    DarknessHasTeeth.logToFile("[Dark] Failed to spawn!");
-                }
-            } catch (Exception e) {
-                DarknessHasTeeth.logToFile("[Dark] Error spawning: " + e.getMessage());
-            }
+            npcPlugin.spawnEntity(store, roleIndex, position, rotation, null, null);
         });
+    }
+
+    private void playSound(@Nonnull final World world, @Nonnull final Store<EntityStore> store,
+            @Nonnull final Ref<EntityStore> playerRef, @Nonnull final Vector3d position, @Nonnull final String sound) {
+        int index = SoundEvent.getAssetMap().getIndex(sound);
+        world.execute(() -> {
+            SoundUtil.playSoundEvent2dToPlayer(playerRef, index, SoundCategory.Ambient, store, 0.5F, 1.2F);
+        });
+    }
+
+    @Nonnull
+    private String getSound() {
+        int index = random.nextInt(sounds.length);
+        return sounds[index];
     }
 
     @Nullable
